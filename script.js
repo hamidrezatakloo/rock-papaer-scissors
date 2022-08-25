b2ee58ba36430c99e16381278929410da9e981fc
hamidreza <takloohamidreza@gmail.com>
2022-08-25 22:06:20 +0430

    Remove console.log and Add list of choices
    Create array that contains list of choices ('rock','paper' and 'scissors')

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let playerSelection = prompt("Rock,paper or scissors");
 
 function computerSelection(){
     randomIndex = Math.floor(Math.random() * 3)
-    console.log(randomIndex);
+    const choices = ['rock','paper','scissors']
 }
