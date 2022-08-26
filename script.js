9bc1f6e0fcb8649658aafa3faf83ae1ddf90d67d
hamidreza <takloohamidreza@gmail.com>
2022-08-26 15:32:06 +0430

    call playRound function inside for

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function game(){
     
     for (let i = 0; i<5; i++){
         let playerSelection = prompt("Rock,paper or scissors");
+        PlayRound(playerSelection,computerSelection())
     }
 
 }
